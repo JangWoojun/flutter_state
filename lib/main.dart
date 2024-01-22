05b796aa1b875a43da4fb0650f0491cee53b7305
JangWoojun <woojun0107@naver.com>
2024-01-22 10:29:37 +0900

    ExampleStateful 레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,81 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Body(),
+    ),
+  ));
 }
 
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [ExampleStateless(), ExampleStateful(index: 3)],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  const ExampleStateful({required this.index, super.key});
+  final int index;
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    print("호출");
+  }
+
+  @override
+  void dispose() {
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+              return;
+            }
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index / 5),
+          child: Center(
+            child: Text("$_index"),
+          ),
+        ),
+      ),
+    );
+  }
+}
